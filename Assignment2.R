ce3eaf9388bdbc4be525b1221dfdf50967c84137
T&T Development <99367973+terrathedeveloper@users.noreply.github.com>
2022-07-06 14:19:07 -0500

    r assignment 2

---
+++ Assignment2.R
@@ -0,0 +1,13 @@
+# R Assignment 2
+
+# Create a variable 'arr' that holds a list of numbers 1 through 6
+
+# Multiply each item in 'arr' by 4
+
+# Divide each item in 'arr' by 3
+
+# Use the sample method to get a random number from 'arr'
+
+# Round the value of Pi to 2 digits  (pi = 3.14159265359)
+
+# Create a function that simulates the roll of 2 di and returns the sum of both dice rolled
